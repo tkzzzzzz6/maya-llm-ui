d954db81184ebbc889051548673f6967fde5331c
tkzzzzzz6 <tkzzzzzz6@noreply.gitcode.com>
2025-10-06 00:26:27 +0800

    Update Supabase migration script to change project URL and service role key for proper storage access.

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,8 +50,8 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'http://supabase_kong_chatbotui:8000';
-  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImV4cCI6MTk4MzgxMjk5Nn0.EGIM96RAZx35lJzdJsyH-qQwv8Hdp7fsn3W0YpN81IU'; -- full access needed for http request to storage
+  project_url TEXT := 'https://qogthjuffbejgocoonlx.supabase.co';
+  service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvZ3RoanVmZmJlamdvY29vbmx4Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1OTY2Mjg3OSwiZXhwIjoyMDc1MjM4ODc5fQ.W3wzGGPA7v8zHjrKkKr-z8YM-lNZbXonqU_AcZOcdyI'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
   SELECT
